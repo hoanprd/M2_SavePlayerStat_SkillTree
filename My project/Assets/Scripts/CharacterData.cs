3f1a2618d4259af0c48b8ed8d06c99aee2ed3308
hoanprd <tinhprd@gmail.com>
2024-07-04 13:56:21 +0700

    CF 04.07.2024 P1

---
+++ My project/Assets/Scripts/CharacterData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData
+{
+    public string characterName;
+    public int hp;
+    public int pArmor;
+    public int mArmor;
+}
